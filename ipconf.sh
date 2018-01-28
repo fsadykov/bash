91e3e2a096b648208b00d4333ac36bf6633ad0f4
Farkhod <sadykovfarkhod@gmail.com>
2018-01-27 19:43:28 -0600

    Deleted some 1/27/2018

--- ipconf.sh
+++ ipconf.sh
@@ -1,21 +1,92 @@
 #!/bin/bash
- 
-checkip=$(grep IPADDR /etc/sysconfig/network-scripts/ip | awk -F= '{print $1}'
-)
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+file=/etc/sysconfig/network-scripts/ifcfg-eth0 
+#file=/home/template
 
-chnatmask=$(grep NETMASK /etc/sysconfig/network-scripts/ip | awk -F= '{print $1}'
- )
-if [ "$checkip" == "IPADDR" ] 
-then 
-	echo yes 
-else 
-	read -p "Please enter your ip: " ipconf
-	read -p "Please enter your GATEWAY: " gateway
-	read -p "Please enter your DNS: " DNS
-	echo IPADDR=$ipconf >> /etc/sysconfig/network-scripts/ip
-	echo NETMASK=255.255.225.0 >> /etc/sysconfig/network-scripts/ip
-	echo GATEWAY=$gateway >> /etc/sysconfig/network-scripts/ip
-	echo DNS1$DNS >> /etc/sysconfig/network-scripts/ip
-	echo DNS2=8.8.8.8 >> /etc/sysconfig/network-scripts/ip
+
+
+functiondhcp(){
+echo "$(grep -v "IP" $file)" > $file
+echo "$(grep -v "NETMASK" $file)" > $file
+echo "$(grep -v "GATEWAY" $file)" > $file
+echo "$(grep -v "DNS" $file)" > $file
+sed 's/BOOTPROTO=static/BOOTPROTO=dhcp/g' -i $file
+}
+
+
+
+
+
+functionstatic(){
+read -p "${red}Please enter the ipaddress :${reset}" ipaddr
+read -p "${red}Please enter the gateway :${reset}" gateway
+#read -p "Please enter the netmask :" netmask
+netmask=255.255.255.0
+echo "IPADDR=$ipaddr" >> $file
+echo "GATEWAY=$gateway" >> $file
+echo "NETMASK=$netmask" >> $file
+echo "DNS1=8.8.8.8" >> $file
+echo "DNS2=8.8.4.4" >> $file
+sed 's/BOOTPROTO=dhcp/BOOTPROTO=static/g' -i $file
+sed 's/ONBOOT=no/ONBOOT=yes/g' -i $file
+}
+
+functionrestart(){
+read -p "Do you want to restart network yes or no? :" restartuser
+if [ $restartuser == "yes" ] || [ $restartuser == "Yes" ] || [ $restartuser == "YES" ]
+then
+	service network restart
 fi
-service network restart
+
+
+}
+
+functioncheck(){
+echo "${green}######################################################"
+echo "# You install static IP"
+ping -c 4 8.8.8.8
+echo "######################################################"${reset}
+}
+
+
+
+
+
+
+
+echo " ${green}Welcome to Farkhod Sadykov's script ${reset} "
+
+echo "1. Centos 6"
+echo "2. Centos 7"
+read -p "What is your versioning vs kernel version? :" OS
+if [ $OS == "1" ]
+then
+	echo "${green}############################################${reset}"
+	echo "${green}1. DHCP "
+	echo "2. STATIC${reset}"
+	echo "${red}3. quit ${reset}"
+	read -p "Chose options :" ip
+	if [ $ip == "1" ]
+	then
+		functiondhcp
+	elif [ $ip == "2" ]
+	then	
+		functiondhcp
+		functionstatic
+		functionrestart
+		functioncheck
+	fi
+
+
+
+
+
+
+	
+elif [ $OS == "2" ]
+then
+	echo "Centos 7"
+fi 
+
