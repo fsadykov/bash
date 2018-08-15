687d4d9475c61c34cfe65da717bf9e3b4c1db069
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-08-14 23:31:06 -0500

    This script will add users to the ifs

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-HOST='10.0.100.104'
-USER='admin'
-PASSWD='redhat'
-sftp -P 22 $USER@$HOST
-lcd /SFTP/files/
-put /My_scripts/ftp/file.txt 
+#!/usr/bin/env bash
+
+# Make sure listuser file has content first column user second is user size
+
+for user in $(cat listuser.txt | awk '{print $1}'):
+do
+   size=$(grep  $user listuser.txt | awk '{print $2}')
+   # Change echo to your command isi quot
+   # to test error change comand give syntax error comand for example  "is quote  $user $size"
+   # Should show  User error check  and Useranem
+   isi quata quatas create --path /ifs/cifs/-data/HDrive/$user --type directory --hard-treshhold 10GB --advisory-treshhold --containers=yes $size > /dev/null && echo "User "$user" has been added succesefuly" || echo "User error check:" $user
+done
